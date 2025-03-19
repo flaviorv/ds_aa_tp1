2b96a7b1eb286eff1b1409fb11a0f88935bf97d5
flaviorv <frezendevaroto@gmail.com>
2025-03-18 21:23:03 -0300

    Matches method which is an autocomplete. When you enter a prefix, it returns all the words that match.

--- trie.py
+++ trie.py
@@ -61,18 +61,38 @@ class Trie:
         found = self._search(word)
         print(f"Is Word {word} in the trie?", found)
     
+    def matches(self, prefix):
+        response = ""
+        all = []
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return "Not found"
+            response+=char
+            if node.is_end == True:
+                all.append(response)
+            node = node.children[char]
+        self.after_match(node, response, all)
+        return all
+
+    def after_match(self, node, response, all):
+        matched = response
+        keys = list(node.children.keys())
+        if node.is_end == True:
+            all.append(response)
+        for i in range(len(keys)):
+            response += keys[i]
+            self.after_match(node.children[keys[i]], response, all)
+            response = matched
+
 if __name__ == "__main__":
     trie = Trie()
-    words = ["casa", "casado", "carro", "caminhão", "cachorro", "cadeira"]
+    words = ["apple", "apricot", "autocomplete", "best", "banana", "basket", "car", "cat", "cap",
+        "dog", "door", "elephant", "engine", "engineer", "island", "jacket", "keyboard", "house"]
     for word in words:
         trie.insert(word)
     print(trie.all_words())
-    # trie.all_levels()
-    trie.search("casa")
-    trie.search("cas")
-    trie.search("casad")
-    trie.search("casado")
-    trie.search("casadoo")
-    trie.search("cadeira")
-    trie.search("Cadeira")
+    prefixes = ["ap", "a", "ba", "ho", "aa", "ca", "jack"]
+    for prefix in prefixes:
+        print(f"Prefix: {prefix}  Autocomplete: {trie.matches(prefix)}")
     
